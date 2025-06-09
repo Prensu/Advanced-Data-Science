331781081b891b5a057606cf07726fce35bb8dbd
Prensu <dangolprensu17newar@gmail.com>
2025-06-09 10:28:20 +0545

    June 9th

--- FastApi/Pydantic/_serialization_.py
+++ FastApi/Pydantic/_serialization_.py
@@ -1,27 +1,41 @@
 from pydantic import BaseModel
 
+# Define Address model
 class Address(BaseModel):
-
     city: str
     state: str
     pin: str
 
+# Define Patient model with default gender
 class Patient(BaseModel):
-
     name: str
     gender: str = 'Male'
     age: int
     address: Address
 
+# Create an address dictionary
 address_dict = {'city': 'gurgaon', 'state': 'haryana', 'pin': '122001'}
 
+# Create an Address object
 address1 = Address(**address_dict)
 
-patient_dict = {'name': 'nitish', 'age': 35, 'address': address1}
+# Create a patient dictionary with a nested address dictionary
+patient_dict = {
+    'name': 'Sachhyam',
+    'age': 23,
+    'address': {
+        'city': 'Sorakhutte',
+        'state': 'Bagmati',
+        'pin': '44600'
+    }
+}
 
+# Create a Patient object
 patient1 = Patient(**patient_dict)
 
+# Dump the model as a dict
 temp = patient1.model_dump(exclude_unset=True)
 
+# Print results
 print(temp)
 print(type(temp))
